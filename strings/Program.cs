7a94ac1a08e77fc6087fd0a6b9fca9c0bbae5a46
eang sokhim <eang sokhim>
2016-01-15 12:03:37 +0700

    comment

---
+++ strings/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName = "Ivan";
+            string lastName = "Ivanov";
+            Console.WriteLine("Hello, {0}!", firstName);
+
+            string fullName = firstName + " " + lastName;
+            Console.WriteLine("Your full name is {0}.", fullName);
+            Console.ReadLine();
+        }
+    }
+}
