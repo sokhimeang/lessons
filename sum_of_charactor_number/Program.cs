c0ff0b50317114fbb48e20505bdc343900c5e1e6
eang sokhim <eang sokhim>
2016-01-25 19:30:07 +0700

    ផលបូករបស់តួនៃចំនួនលេខ ឧ: 123: result: 1+2+3=6 តាមលំដាប់ពីឆ្វេងទៅស្ដាំ

---
+++ sum_of_charactor_number/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace sum_of_charactor_number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            uint n;
+            Console.Write("enter n: ");
+            n = uint.Parse(Console.ReadLine());
+            uint sum = 0, m;
+
+            Console.Write("result= ");
+            while (n != 0)
+            {
+                m = n % 10;
+                sum += n % 10;
+                if (n < 10)
+                {
+                    Console.Write("{0} = ", m);
+                }
+                else
+                {
+                    Console.Write("{0} + ", m);
+                }
+                n /= 10;
+            }           
+            Console.Write("{0}", sum);
+            Console.ReadLine();
+        }
+    }
+}
